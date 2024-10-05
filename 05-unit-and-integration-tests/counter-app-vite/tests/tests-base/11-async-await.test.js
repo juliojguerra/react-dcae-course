4f9387385ac085c294df9879bbf41a4e9574eaa2
Julio Guerra <j.guerratamayo@gmail.com>
2024-10-05 15:56:08 -0500

    add FirstApp

--- 05-unit-and-integration-tests/counter-app-vite/tests/tests-base/11-async-await.test.js
+++ 05-unit-and-integration-tests/counter-app-vite/tests/tests-base/11-async-await.test.js
@@ -1,7 +1,7 @@
 import { getImagen } from "../../src/base-pruebas/11-async-await";
 
 describe("Tests in 11-async-await.js", () => {
-  test("getImage should return the image URL", async () => {
+  test.skip("getImage should return the image URL", async () => {
     const url = await getImagen();
     console.log(url);
 
